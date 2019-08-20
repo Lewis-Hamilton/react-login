944144b88da783c1ba4a12f2b98fe74a0296413e
unknown <hamiltonjlewis@gmail.com>
2019-08-19 19:47:11 -0500

    login-works

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
-//import fire from './config/Fire';
+import fire from './config/Fire';
 
 class Login extends Component {
     constructor(props){
       super(props);
+      this.login = this.login.bind(this);
+      this.handleChange = this.handleChange.bind(this);
       this.state = {
         email:'',
         password:''
       }
+    }
+
+    login(e) {
+      e.preventDefault();
+      fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+      }).catch((error) => {
+        console.log(error);
+      });
     }
 
     handleChange(e){
